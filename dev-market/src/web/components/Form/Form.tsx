60afec776731f11d5be889f2616eb1e42bd91ce6
Guilherme-SilvaH <guihenriquesilva10@gmail.com>
2024-04-01 18:23:50 -0300

    dando vida - Form

--- dev-market/src/web/components/Form/Form.tsx
+++ dev-market/src/web/components/Form/Form.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import { Button, FormGroup, Input, Label, Form as RsForm } from 'reactstrap';
+import { FormEvent } from 'react';
+import { Button, Form as FormGroup, Input, Label } from 'reactstrap';
 
-
-// Definindo as props do componente
 interface FormProps {
-    mode: 'login' | 'signup';
+    mode: string;
     onSubmit: (email: string, password: string) => void;
 }
 
-export default function Form ({ mode, onSubmit }: FormProps) {
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+export default function Form({ mode, onSubmit }: FormProps) {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const target = event.target as typeof event.target & {
             email: { value: string };
@@ -21,41 +19,37 @@ export default function Form ({ mode, onSubmit }: FormProps) {
     };
 
     return (
-        <div>
-            <RsForm onSubmit={handleSubmit}>
-            <h2>{mode === 'login' ? 'Login' : 'Cadastro'}</h2> 
-                <FormGroup floating className='inputform'>
-                    <Input
-                        
-                        id="email"
-                        name="email"
-                        placeholder="Email"
-                        type="email"
-                        required
-                    />
-                    <Label for="email">Email</Label>
-                </FormGroup>
-               
-                <FormGroup floating>
-                    <Input
-                        id="password"
-                        name="password"
-                        placeholder="Password"
-                        type="password"
-                        required
-                    />
-                    <Label for="password">Password</Label>
-                </FormGroup>
-               
-                <Button type="submit">
-                    {mode === 'login' ? 'Login' : 'Sign Up'}
-                </Button>
-            </RsForm>
+        <form onSubmit={handleSubmit}>
+            <div id='container-titles'>
+                <h1>{mode === 'login' ? 'Login' : 'Cadastro'}</h1>
+                <h2>Comece sem pagar nada</h2>
+            </div>
+
+            <FormGroup>
+                <Label for="Email">Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="example@gmail.com" />
+            </FormGroup>
+
+            <FormGroup>
+                <Label for="Password">Password</Label>
+                <Input type="password" name="password" id="examplePassword" placeholder="123456789" />
+            </FormGroup>
+
+            <FormGroup check>
+                <Label check>
+                    <Input type="checkbox" />{' '}
+                    Check me out
+                </Label>
+            </FormGroup>
+
+            <Button className='container-button' type="submit" >
+                {mode === 'login' ? 'Login' : 'Sign Up'}
+            </Button>
             {mode === 'login' ? (
-                <p>Não tem uma conta? <a href="/signup">Cadastre-se</a></p>
+                <p className='container-p'>Não tem uma conta? <a href="/signup">Cadastre-se</a></p>
             ) : (
-                <p>Já tem uma conta? <a href="/login">Login</a></p>
+                <p className='container-p'>Já tem uma conta? <a href="/login">Login</a></p>
             )}
-        </div>
+        </form>
     );
 }
